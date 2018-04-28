f59df046a02c606f541b84ea7a995ce069639223
igilfu <33724031+igilfu@users.noreply.github.com>
2018-04-28 20:17:23 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -187,32 +187,22 @@ inline const CircularInt operator+ (const CircularInt& c1, const CircularInt& c2
     }
     return temp;
 }
-inline const CircularInt operator/(const CircularInt& c1, const CircularInt& c2){
-    if(c1.min>INT16_MIN&&c1.max>INT16_MIN&&c1.min!=c1.max){
-        if(c1.hour%c2.hour!=0)
-            throw string("There is no number x in {"+to_string(c1.min)+","+to_string(c1.max)+"} such that x*"+to_string(c2.hour)+"="+to_string(c1.hour));
-    }
-    else{
-        if(c1.hour%c2.hour!=0)
-            throw string("There is no number x in {"+to_string(c2.min)+","+to_string(c2.max)+"} such that x*"+to_string(c2.hour)+"="+to_string(c1.hour));
-    }
-    CircularInt temp(c1.hour/c2.hour);
-    if(c1.min>INT16_MIN&&c1.max>INT16_MIN&&c1.min!=c1.max){
-        while(temp.hour<c1.min){
-            temp.hour=temp.hour+(c1.max-c1.min+1);
+inline const CircularInt operator/(const CircularInt& c, const CircularInt& other){
+     if(c.hour%other.hour!=0)
+            throw string("There is no number x in {"+to_string(c.min)+","+to_string(c.max)+"} such that x*"+to_string(other.hour)+"="+to_string(c.hour));
+
+    CircularInt temp(c);
+    bool flag=true;
+    int i=temp.min;
+        while(i<=temp.max && flag){
+            if(i*other.hour == temp.hour){
+                temp.hour=i;
+                flag=false;
+            }
+            i++;
         }
-        while(temp.hour>c1.max){
-            temp.hour=temp.hour-(c1.max-c1.min+1);
-        }
-    }
-    else{
-        while(temp.hour<c2.min){
-            temp.hour=temp.hour+(c2.max-c2.min+1);
-        }
-        while(temp.hour>c2.max){
-            temp.hour=temp.hour-(c2.max-c2.min+1);
-        }
-    }
+             if(flag)
+        throw invalid_argument("THERE IS NO SUCH NUMBER!");
     return temp;
 }
 inline const CircularInt operator/(const CircularInt& c,const int other){
@@ -234,12 +224,21 @@ inline const CircularInt operator/(const CircularInt& c,const int other){
     return temp;
 }
 inline const CircularInt operator/(const int other,const CircularInt& c){
+     if(c.hour%other!=0)
+            throw string("There is no number x in {"+to_string(c.min)+","+to_string(c.max)+"} such that x*"+to_string(other)+"="+to_string(c.hour));
+
     CircularInt temp(c);
-    temp.hour=temp.hour/other;
-        while(temp.hour<temp.min)
-            temp.hour=temp.hour+(temp.max-temp.min+1);
-        while(temp.hour>temp.max)
-            temp.hour=temp.hour-(temp.max-temp.min+1);
+    bool flag=true;
+    int i=temp.min;
+        while(i<=temp.max && flag){
+            if(i*other == temp.hour){
+                temp.hour=i;
+                flag=false;
+            }
+            i++;
+        }
+             if(flag)
+        throw invalid_argument("THERE IS NO SUCH NUMBER!");
     return temp;
 }
 inline const CircularInt operator*(const CircularInt& c1, const CircularInt& c2){
